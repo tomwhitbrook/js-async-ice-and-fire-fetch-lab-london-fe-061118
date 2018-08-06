a359198e243f948616c96c9bb17dec21079233cd
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-08-06 17:13:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
+}
+
+function fetchHouses() {
+  //write fetch request to the Game of Thrones API
+
+  fetch('https://anapioficeandfire.com/api/houses')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
+}
+
+function fetchFifthBook() {
+  //write fetch request to the Game of Thrones API
+
+  fetch('https://anapioficeandfire.com/api/books/5')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
+}
+
+function fetch1031Char() {
+  //write fetch request to the Game of Thrones API
+
+  fetch('https://anapioficeandfire.com/api/characters/1031')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
 }
 
 function renderBooks(json) {
@@ -11,6 +43,22 @@ function renderBooks(json) {
   })
 }
 
+function fetchHousesAll() {
+  //write fetch request to the Game of Thrones API
+
+  for(var nof=1; nof<=number_of_result_pages;nof++)
+  var URL='https://www.anapioficeandfire.com/api/characters?page='+nof+'&per_page=10';
+
+  fetch(URL)
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+  fetchHouses()
+  fetchFifthBook()
+  fetch1031Char()
+  fetchHousesAll()
 })
